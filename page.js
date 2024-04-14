18ed28787a7f7879551ca3a15c4450f39a76bb52
William Clifford <w.big.red.dog@gmail.com>
2024-04-13 23:56:55 -0500

    implemented js getBoard method for websocket cals

--- page.js
+++ page.js
@@ -17,40 +17,40 @@ function drawBoard(ctx, pieces){
             if(pieces[i][j] == '.'){
                 continue;
             }
-            else if(pieces[i][j].name === "wpawn"){
+            else if(pieces[i][j].name === "P"){
                 ctx.drawImage(wpawn, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "wrook"){
+            else if(pieces[i][j].name == "R"){
                 ctx.drawImage(wrook, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "wknight"){
+            else if(pieces[i][j].name == "N"){
                 ctx.drawImage(wknight, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "wbish"){
+            else if(pieces[i][j].name == "B"){
                 ctx.drawImage(wbish, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "wqueen"){
+            else if(pieces[i][j].name == "Q"){
                 ctx.drawImage(wqueen, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "wking"){
+            else if(pieces[i][j].name == "K"){
                 ctx.drawImage(wking, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "bpawn"){
+            else if(pieces[i][j].name == "p"){
                 ctx.drawImage(bpawn, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "brook"){
+            else if(pieces[i][j].name == "r"){
                 ctx.drawImage(brook, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "bknight"){
+            else if(pieces[i][j].name == "n"){
                 ctx.drawImage(bknight, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "bbish"){
+            else if(pieces[i][j].name == "b"){
                 ctx.drawImage(bbish, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "bqueen"){
+            else if(pieces[i][j].name == "q"){
                 ctx.drawImage(bqueen, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
-            else if(pieces[i][j].name == "bking"){
+            else if(pieces[i][j].name == "k"){
                 ctx.drawImage(bking, pieces[i][j].x, pieces[i][j].y, pW, pH);
             }
             else{
@@ -60,7 +60,7 @@ function drawBoard(ctx, pieces){
         }
         // ctx.drawImage(pieces[i].image, pieces[i].x, pieces[i].y, pW, pH);
     }
-    console.log(pieces);
+    //console.log(pieces);
 }
 
 var board = document.getElementById("board");
@@ -97,7 +97,7 @@ for(var i = 0; i < 8; i++){
 var wpawn = new Image();
 wpawn.onload = function () {
     for (var j = 0; j < 8; j++) {
-        pieces[6][j] = new Piece('wpawn',wpawn,j * (board.width / 8),6 * (board.height / 8));
+        pieces[6][j] = new Piece('P',wpawn,j * (board.width / 8),6 * (board.height / 8));
         ctx.drawImage(wpawn,j * (board.width / 8),6 * (board.height / 8),board.width / 8, board.height / 8);
     }
 };
@@ -105,41 +105,42 @@ wpawn.src = "images/wP.svg";
 
 var wrook = new Image();
 wrook.onload = function() {
-    pieces[7][0] = new Piece('wrook',wrook, 0 * (board.width / 8), 7 * (board.height / 8));
+    pieces[7][0] = new Piece('R',wrook, 0 * (board.width / 8), 7 * (board.height / 8));
+    //console.log(pieces[7][0].name);
     ctx.drawImage(wrook,0 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[7][7] = new Piece('wrook',wrook, 7 * (board.width / 8),7 * (board.height / 8));
+    pieces[7][7] = new Piece('R',wrook, 7 * (board.width / 8),7 * (board.height / 8));
     ctx.drawImage(wrook,7 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
 };
 wrook.src = "images/wR.svg";
 
 var wknight = new Image();
 wknight.onload = function() {
-    pieces[7][1] = new Piece('wknight',wknight, 1 * (board.width / 8), 7 * (board.height / 8));
+    pieces[7][1] = new Piece('N',wknight, 1 * (board.width / 8), 7 * (board.height / 8));
     ctx.drawImage(wknight,1 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[7][6] = new Piece('wknight',wknight, 6 * (board.width / 8), 7 * (board.height / 8));
+    pieces[7][6] = new Piece('N',wknight, 6 * (board.width / 8), 7 * (board.height / 8));
     ctx.drawImage(wknight,6 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
 };
 wknight.src = "images/wN.svg";
 
 var wbish = new Image();
 wbish.onload = function() {
-    pieces[7][2] = new Piece('wbish',wbish,2 * (board.width / 8),7 * (board.height / 8));
+    pieces[7][2] = new Piece('B',wbish,2 * (board.width / 8),7 * (board.height / 8));
     ctx.drawImage(wbish,2 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[7][5] = new Piece('wbish',wbish,5 * (board.width / 8),7 * (board.height / 8));
+    pieces[7][5] = new Piece('B',wbish,5 * (board.width / 8),7 * (board.height / 8));
     ctx.drawImage(wbish,5 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
 };
 wbish.src = "images/wB.svg";
 
 var wqueen = new Image();
 wqueen.onload = function () {
-    pieces[7][3] = new Piece('wqueen',wqueen,3 * (board.width / 8),7 * (board.height / 8));
+    pieces[7][3] = new Piece('Q',wqueen,3 * (board.width / 8),7 * (board.height / 8));
     ctx.drawImage(wqueen,3 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
 }
 wqueen.src = "images/wQ.svg";
 
 var wking = new Image();
 wking.onload = function () {
-    pieces[7][4] = new Piece('wking',wking,4 * (board.width / 8),7 * (board.height / 8));
+    pieces[7][4] = new Piece('K',wking,4 * (board.width / 8),7 * (board.height / 8));
     ctx.drawImage(wking,4 * (board.width / 8),7 * (board.height / 8),board.width / 8, board.height / 8);
 }
 wking.src = "images/wK.svg";
@@ -148,7 +149,7 @@ wking.src = "images/wK.svg";
 var bpawn = new Image();
 bpawn.onload = function () {
     for (var j = 0; j < 8; j++) {
-        pieces[1][j] = new Piece('bpawn',bpawn,j * (board.width / 8),1 * (board.height / 8));
+        pieces[1][j] = new Piece('p',bpawn,j * (board.width / 8),1 * (board.height / 8));
         ctx.drawImage(bpawn,j * (board.width / 8),1 * (board.height / 8),board.width / 8, board.height / 8);
     }
 };
@@ -156,41 +157,41 @@ bpawn.src = "images/bP.svg";
 
 var brook = new Image();
 brook.onload = function() {
-    pieces[0][0] = new Piece('brook',brook,0 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][0] = new Piece('r',brook,0 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(brook,0 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[0][7] = new Piece('brook',brook,7 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][7] = new Piece('r',brook,7 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(brook,7 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
 };
 brook.src = "images/bR.svg";
 
 var bknight = new Image();
 bknight.onload = function() {
-    pieces[0][1] = new Piece('bknight',bknight,1 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][1] = new Piece('n',bknight,1 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bknight,1 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[0][6] = new Piece('bknight',bknight,6 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][6] = new Piece('n',bknight,6 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bknight,6 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
 };
 bknight.src = "images/bN.svg";
 
 var bbish = new Image();
 bbish.onload = function() {
-    pieces[0][2] = new Piece('bbish',bbish,2 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][2] = new Piece('b',bbish,2 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bbish,2 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
-    pieces[0][5] = new Piece('bbish',bbish,5 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][5] = new Piece('b',bbish,5 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bbish,5 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
 };
 bbish.src = "images/bB.svg";
 
 var bqueen = new Image();
 bqueen.onload = function () {
-    pieces[0][3] = new Piece('bqueen',bqueen,3 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][3] = new Piece('q',bqueen,3 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bqueen,3 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
 }
 bqueen.src = "images/bQ.svg";
 
 var bking = new Image();
 bking.onload = function () {
-    pieces[0][4] = new Piece('bking',bking,4 * (board.width / 8),0 * (board.height / 8));
+    pieces[0][4] = new Piece('k',bking,4 * (board.width / 8),0 * (board.height / 8));
     ctx.drawImage(bking,4 * (board.width / 8),0 * (board.height / 8),board.width / 8, board.height / 8);
 }
 bking.src = "images/bK.svg";
@@ -202,8 +203,6 @@ bking.src = "images/bK.svg";
 // NEED TO FIGURE OUT PROCESS FLOW
 // ASYNCHRONOUS NATURE OF EVENT HANDLERS MAKES IT DIFFICULT
 // I don't think the semaphore method will work correctly since the code might be arbitrarily executed
-// wrap everything in a class called game
-    // game will have a board, pieces, currplayer
 var currP;
 var tempCoord; // use to track square that clicked piece currently occupies
 var res;
@@ -225,7 +224,17 @@ ws.addEventListener("message", (event) => {
 
     switch(res.method){
         case "ismovelegal":
-            legalityCheck(res);
+            if(res.result == true && currP !== '.'){
+                currP.isDragging = false;
+        
+                currP.x = Math.floor((currP.x + (pW / 2)) / pW) * pW;
+                currP.y = Math.floor((currP.y + (pH / 2)) / pH) * pH;
+        
+                pieces[Math.floor(mouseY / pH)][Math.floor(mouseX/ pW)] = currP;
+            }
+            else{
+                pieces[tempCoord[0]][tempCoord[1]] == currP;
+            }
             break;
         case "getminimaxmove":
             minimaxMove(res);
@@ -233,18 +242,6 @@ ws.addEventListener("message", (event) => {
         case "geteval":
             showEval(res);
             break;
-    }
-
-    if(res.result == true && currP !== '.'){
-        currP.isDragging = false;
-
-        currP.x = Math.floor((currP.x + (pW / 2)) / pW) * pW;
-        currP.y = Math.floor((currP.y + (pH / 2)) / pH) * pH;
-
-        pieces[Math.floor(mouseY / pH)][Math.floor(mouseX/ pW)] = currP;
-    }
-    else{
-        pieces[tempCoord[0]][tempCoord[1]] == currP;
     }
 });
 
@@ -265,12 +262,18 @@ board.addEventListener('mouseup', function(event){
     var mouseX = event.clientX - board.getBoundingClientRect().left;
     var mouseY = event.clientY - board.getBoundingClientRect().top;
 
-    // check if move is legal
-    //ws.send();
+    currP.isDragging = false;
 
-    while(waiting){
-        sleep(10);
-    }
+    currP.x = Math.floor((currP.x + (pW / 2)) / pW) * pW;
+    currP.y = Math.floor((currP.y + (pH / 2)) / pH) * pH;
+
+    pieces[Math.floor(mouseY / pH)][Math.floor(mouseX/ pW)] = currP;
+
+    console.log(getBoard(tempBoard));
+
+    // while(waiting){
+    //     sleep(10);
+    // }
 
     drawBoard(ctx,pieces);
 });
@@ -295,21 +298,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//check for checkmate in here
-function legalityCheck(data){
-    
-    if(res.isCheckmate == true){
-        isCheckmate = true;
+function getBoard(){
+    let pieceArray = [[],[],[],[],[],[],[],[]];
+    for(let i = 0; i < 8; ++i){
+        for(let j = 0; j < 8; ++j){
+            if(pieces[i][j] != '.'){
+                pieceArray[i][j] = pieces[i][j].name;
+            }
+            else{
+                pieceArray[i][j] = '.';
+            }
+        }
     }
-    waiting = false;
-}
-
-function minimaxMove(){
-
-}
-
-function showEval(){
-
+    return pieceArray;
 }
 
 drawBoard(ctx,pieces);
@@ -321,7 +322,7 @@ newGameButton.addEventListener("click", () => {
     choiceDiv.innerHTML = "<button id=\"whiteButton\">white</button><button id=\"blackButton\">black</button>"
     whiteButton = document.getElementById("whiteButton");
     blackButton = document.getElementById("blackButton");
-    console.log(choiceDiv.innerHTML)
+    //console.log(choiceDiv.innerHTML)
     whiteButton.addEventListener("click", () => {
         console.log("white button clicked");
         isCheckmate = false;
