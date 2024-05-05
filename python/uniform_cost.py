62763ffc75d59629b8641564dc3c19d90647f6f2
William Clifford <w.big.red.dog@gmail.com>
2024-05-05 13:55:40 -0500

    removed unnecessary files

--- python/uniform_cost.py
+++
@@ -1,3 +0,0 @@
-# probably going to need to write a new implementation for UCS with customizable inputs
-# graph information can be passed in through websocket
-# should set up front end js with canvas before 
